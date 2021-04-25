c13114a6be28023cd0b62d1594b58d72e5b3869a
alejandroarribas <alejandro.arribas@live.u-tad.com>
2021-04-25 21:55:49 +0200

    Subida de sistemaFuncional

---
+++ Proyectos_4_UnExtinsion/Assets/Scripting/ProyectilAiChase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProyectilAiChase : MonoBehaviour
+{
+    Rigidbody rb;
+    SphereCollider sc;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        sc = GetComponent<SphereCollider>();
+    }
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.isKinematic = true;
+        sc.isTrigger = true;
+        sc.radius = 3;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<AiBehaivour>())
+        {
+            other.GetComponent<AiBehaivour>().detected = true;
+            other.GetComponent<AiBehaivour>().Position = this.transform;
+            other.GetComponent<AiBehaivour>().Object = this.gameObject;
+        }
+    }
+}
