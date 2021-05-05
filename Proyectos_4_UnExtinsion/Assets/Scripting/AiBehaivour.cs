726bf794b5a39724c53187c210bb563421a9894d
Adrián Domínguez <adrian.dominguez.diaz@gmail.com>
2021-05-05 14:02:57 +0200

    Recuperada escena de Alejandro

--- Proyectos_4_UnExtinsion/Assets/Scripting/AiBehaivour.cs
+++ Proyectos_4_UnExtinsion/Assets/Scripting/AiBehaivour.cs
@@ -6,41 +6,115 @@ using UnityEngine.AI;
 public class AiBehaivour : MonoBehaviour
 {
     NavMeshAgent Agent;
+    Animator Anim;
     public GameObject Player;
+    public GameObject Path;
+    public float speed = 2;
+    public GameObject[] Positions;
+    public GameObject Object;
+    public int ActualPoint;
     public float FieldOfVew;
     public bool detected;
     public float DetectDistance;
+    public float Timer = 2;
+    public Transform Position;
+    float ActualTimer;
     // Start is called before the first frame update
     void Start()
     {
+        detected = false;
         Agent = GetComponent<NavMeshAgent>();
+        Anim = GetComponent<Animator>();
+        Player = FindObjectOfType<PalyerMovment>().gameObject;
+        Positions = new GameObject[Path.transform.childCount];
+        for (int i = 0; i < Positions.Length; i++)
+        {
+            Positions[i] = Path.transform.GetChild(i).gameObject;
+        }
+        ActualTimer = Timer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position,Player.transform.position)<=DetectDistance)
+        Animations();
+        if (!detected)
+        {
+            FollowPath();
+            DetectPlayer();
+        }
+        if(detected)
+        {
+            FollowPosition();
+        }
+
+    }
+    public void DetectPlayer()
+    {
+        if (Vector3.Distance(transform.position, Player.transform.position) <= DetectDistance)
         {
             Vector3 Direction = Player.transform.position - transform.position;
             float angle = Vector3.Angle(Direction, transform.forward);
-            if(angle<FieldOfVew*0.5f)
+            if (angle < FieldOfVew * 0.5f)
             {
                 RaycastHit Out;
-                if(Physics.Raycast(transform.position,Direction,out Out,DetectDistance))
+                if (Physics.Raycast(transform.position, Direction, out Out, DetectDistance))
                 {
-                    if(Out.transform.gameObject == Player)
+                    if (Out.transform.gameObject == Player)
                     {
                         detected = true;
-                        Agent.SetDestination(Player.transform.position);
+                        DetectPlayer();
                     }
                 }
             }
 
         }
     }
-    public void DetectPlayer()
+    void FollowPath()
     {
-
+        Agent.SetDestination(Positions[ActualPoint].transform.position);
+        if(Vector3.Distance(transform.position,Positions[ActualPoint].transform.position)<1)
+        {
+            Agent.speed = 0;
+            ChangePosition();
+            
+        }
+    }
+    void ChangePosition()
+    {
+        ActualTimer -= Time.deltaTime;
+        if (ActualTimer <= 0)
+        {
+            Agent.speed = speed;
+            ActualPoint += 1;
+            if (ActualPoint >= Positions.Length)
+            {
+                ActualPoint = 0;
+            }
+            ActualTimer = Timer;
+        }
+    }
+    void Animations()
+    {
+        Anim.SetFloat("Speed", Agent.speed);
+    }
+    void FollowPosition()
+    {
+        Agent.speed = speed;
+        Agent.SetDestination(Position.position);
+       
+        if (Vector3.Distance(transform.position, Position.position) <= 0.2f)
+        {
+            Agent.speed = 0;
+            ActualTimer -= Time.deltaTime;
+            if (ActualTimer <= 0)
+            {
+                Agent.speed = speed;
+                detected = false;
+                Destroy(Object);
+                ActualTimer = Timer;
+            }
+        }
     }
     
 }
--- Proyectos_4_UnExtinsion/Assets/Scripting/PalyerMovment.cs
+++ Proyectos_4_UnExtinsion/Assets/Scripting/PalyerMovment.cs
@@ -4,34 +4,94 @@ using UnityEngine;
 
 public class PalyerMovment : MonoBehaviour
 {
+    [Header("Movement Var")]
     public float Speed;
     public float JumpForce;
     float RotateSpeed;
     public float TimeRotate;
-    CharacterController Char;
+    public float Gravity;
+   
+    [Header("TestVar")]
     public Transform CameraTransform;
     public Vector3 Move;
-    Vector3 Direction;
+    public bool Grounded;
+    [Header("Detectors")]
+    public Vector3 FloorDetector;
+    public Vector3 newFloorDetection;
+    public float floorDistance;
+
+    Rigidbody Rb;
+    public Vector3 movimiento;
+    public Vector3 movimientoHorizontal;
+    public GameObject Priyectil;
+    public GameObject Instancia;
+    public Transform Spawn;
     // Start is called before the first frame update
     void Start()
     {
-        Char = GetComponent<CharacterController>();
+        Rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        newFloorDetection = transform.forward;
+        FloorDetector = -transform.up;
         float Xmove = Input.GetAxis("Horizontal");
         float Zmove = Input.GetAxis("Vertical");
+        float MouseX = Input.GetAxis("Mouse X");
         Move = new Vector3(Xmove, 0, Zmove).normalized;
-       
-        
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instancia = Instantiate(Priyectil, Spawn.position, Quaternion.identity);
+            Instancia.GetComponent<Rigidbody>().AddForce((transform.forward + transform.up) * 300);
+        }
+        transform.Rotate(0, MouseX, 0);
         if (Xmove!=0||Zmove!=0)
         {
-            float AngleTo = Mathf.Atan2(Move.x, Move.z) * Mathf.Rad2Deg + CameraTransform.eulerAngles.y;
-            float AngleFinal = Mathf.SmoothDampAngle(transform.eulerAngles.y, AngleTo, ref RotateSpeed, TimeRotate);
-            transform.rotation = Quaternion.Euler(0, AngleFinal, 0);
-            Char.Move(transform.forward * Speed * Time.deltaTime);
+            //float AngleTo = Mathf.Atan2(Move.x, Move.z) * Mathf.Rad2Deg + CameraTransform.eulerAngles.y;
+            //float AngleFinal = Mathf.SmoothDampAngle(transform.eulerAngles.y, AngleTo, ref RotateSpeed, TimeRotate);
+            //transform.rotation = Quaternion.Euler(0, AngleFinal, 0);
+            movimientoHorizontal = transform.right * Xmove+transform.forward*Zmove; 
+        }
+        else
+        {
+            movimientoHorizontal = Vector3.zero;
+        }
+        if (!Grounded)
+        {
+            movimiento.y -= Gravity * Time.deltaTime;
+        }
+        else
+        {
+            movimiento.y = 0;
+        }
+        if(Input.GetKeyDown(KeyCode.Space)&&Grounded)
+        {
+            movimiento.y = JumpForce*Time.deltaTime;
+        }
+        Rb.velocity=(movimiento+movimientoHorizontal);
+    }
+    private void FixedUpdate()
+    {
+        Ray ForwardDetection = new Ray(transform.position+transform.up*0.5f, newFloorDetection);
+        Ray FloorDetection = new Ray(transform.position, FloorDetector);
+        RaycastHit FloorHit;
+        RaycastHit NewFloorHit;
+        if(Physics.Raycast(FloorDetection,out FloorHit,floorDistance))
+        {
+            Debug.Log("Suelo");
+            Grounded = true;
+        }
+        else
+        {
+            Grounded = false;
+        }
+        if(Physics.Raycast(ForwardDetection, out NewFloorHit, floorDistance))
+        {
+            transform.up = NewFloorHit.normal;
+            transform.position = Vector3.MoveTowards(transform.position,NewFloorHit.point,0.2f);
         }
     }
 }
